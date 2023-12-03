5d0a9e2d1b30d620a7e1e2a56dc0db762eac8e09
Roland Papdi <rolee15@gmail.com>
2023-12-03 21:25:27 +0100

    Task 3 solved

--- bead/bead.hs
+++ bead/bead.hs
@@ -1,4 +1,5 @@
 import Data.Char
+import Data.List
 import Numeric
 
 unsafeLookup :: Eq a => a -> [(a,b)] -> b
@@ -17,10 +18,17 @@ parseStringOfBase = map . parseCharOfBase
 parseInteger :: Int -> String -> Integer
 parseInteger b x
     | b < 2 || b > 16 = error ("parseInteger: Invalid base " ++ show b)
-    | x == ""         = error "parseInteger: Empty string"
-    | otherwise       = sum (map (* toInteger b) (parseStringOfBase (toInteger b) x))
+    | all isSpace x   = error "parseInteger: Empty string"
+    | otherwise       = foldl' (\acc digit -> acc * toInteger b + toInteger digit) 0 (parseStringOfBase (toInteger b) x)
 
--- [a0, a1, a2, ..., ak], length: k+1
--- ak * 16^k + ... + a2 * 16^2 + a1 * 16^1 + a0 * 16^0
+-- [a0, a1, a2, ..., ak], length: k+1, base: b
+-- ak * b^k + ... + a2 * b^2 + a1 * b^1 + a0 * b^0
 -- or
--- a0 * 16^0 + a1 * 16^1 + a2 * 16^2 + ... + ak * 16^k
+-- a0 * b^0 + a1 * b^1 + a2 * b^2 + ... + ak * b^k
+-- can be reoreder to
+-- a0 + b(a1 + b(a2 + ... (ak-1 + b * ak)))
+-- e.g.
+-- 12 + 10 * 16 + 15 * 16^2 + 0 * 16^3
+
+hexToDecimal :: [Int] -> Integer
+hexToDecimal = foldl' (\acc digit -> acc * 16 + toInteger digit) 0
