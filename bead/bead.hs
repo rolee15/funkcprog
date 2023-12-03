11789c61cac7cd83b276a506dc185718d3806084
rolee15 <rolee15@gmail.com>
2023-12-03 19:00:34 +0100

    Task 3 partial solution

--- bead/bead.hs
+++ bead/bead.hs
@@ -9,8 +9,18 @@ unsafeLookup k ((x1,x2):xs) = if k == x1 then x2
 parseCharOfBase :: Integer -> Char -> Integer
 parseCharOfBase b x
     | b < 2 || b > 16              = error ("parseCharOfBase: Invalid base " ++ show b)
-    | toInteger(digitToInt x) >= b = error ("parseCharOfBase: Digit is invalid in given base: " ++ show x)
+    | toInteger(digitToInt x) >= b = error ("parseCharOfBase: Digit is invalid in given base " ++ show x)
     | otherwise                    = toInteger(digitToInt x)
 
+parseStringOfBase = map . parseCharOfBase
+
 parseInteger :: Int -> String -> Integer
-parseInteger = undefined
+parseInteger b x
+    | b < 2 || b > 16 = error ("parseInteger: Invalid base " ++ show b)
+    | x == ""         = error "parseInteger: Empty string"
+    | otherwise       = sum (map (* toInteger b) (parseStringOfBase (toInteger b) x))
+
+-- [a0, a1, a2, ..., ak], length: k+1
+-- ak * 16^k + ... + a2 * 16^2 + a1 * 16^1 + a0 * 16^0
+-- or
+-- a0 * 16^0 + a1 * 16^1 + a2 * 16^2 + ... + ak * 16^k
